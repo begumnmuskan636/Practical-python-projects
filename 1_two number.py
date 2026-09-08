ce40afcc962993b244ce9e1ce6b67fdb8406a205
Muskan Begum <begumnmuskan636@gmail.com>
2026-09-08 11:47:38 +0530

    addin 2 files

---
+++ 1_two number.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print("Sum:" , a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+=======
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+
+print("Sum:" , a + b)
+print("Difference:", a - b)
+print("Product:", a * b)
+print("Quotient:", a / b)
+>>>>>>> da8e2f0 (first commit)
+print("Remainder:", a % b)
---
+++ 2_area of circle.py
@@ -0,0 +1,8 @@
+<<<<<<< HEAD
+r=float(input ("Enter the radius:"))
+area=3.14159 * r * r
+=======
+r=float(input ("Enter the radius:"))
+area=3.14159 * r * r
+>>>>>>> da8e2f0 (first commit)
+print("Enter the area of circle", area)
